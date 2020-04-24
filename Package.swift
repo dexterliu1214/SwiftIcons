9740d27ea7290d53b0983024b4655555a2884486
Dexter Liu <dexterliu1214@gmail.com>
2020-04-24 14:45:16 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,9 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftIcons",
-            path: "Source",
             dependencies: [
                 
             ]),
+            path: "Source",
     ]
 )
